a59b4451aafd7bdd6bbfd4b537cb97bfea8b4f20
DharsDev <dharshanpalani.2006@gmail.com>
2024-10-24 14:33:30 +0530

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './css/App.css'
+import ProjectList from './Project-List'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+      <div className="box box-1">
+        <div className="box box-1-5">
+          <h1>Projects</h1>
+          <ProjectList></ProjectList>
+        </div>
+      </div>
+      <div className="box box-2">
+      <div className="box box-2-5">
+        <h1>Dharshan Palani</h1>
+      </div>
+      </div>
+      <div className="box box-3">
+        <div className="box box-3-5"></div>
+      </div>
+      <div className="box box-4">
+        <div className="box box-4-5"></div>
+      </div>
+      <div className="box box-5">
+        <div className="box box-5-5"></div>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Project-List.jsx
@@ -0,0 +1,22 @@
+import './css/Project-List.css'
+
+function ProjectList() {
+    return(<>
+
+        <ul  className="project-list">
+        <li><div className="list-box"></div></li>
+        <li><div className="list-box"></div></li>
+        <li><div className="list-box"></div></li>
+        </ul>
+
+        <ul  className="project-list">
+        <li><div className="list-box"></div></li>
+        <li><div className="list-box"></div></li>
+        <li><div className="list-box"></div></li>
+        </ul>
+    
+    </>)
+}
+
+
+export default ProjectList
